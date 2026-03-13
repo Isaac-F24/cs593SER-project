1a27a30809aca5bed6906bfc71b5c9a39d399323
Isaac-F24 <isaacfuksman@gmail.com>
2026-03-13 04:18:05 +0000

    world with two robots

--- cs593/launch/visualize_franka_robot.launch.py
+++ cs593/launch/visualize_franka_robot.launch.py
@@ -30,38 +30,38 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import  LaunchConfiguration
 from launch_ros.actions import Node
 
-def get_robot_description(context: LaunchContext, robot_type, load_gripper, franka_hand):
-    robot_type_str = context.perform_substitution(robot_type)
-    load_gripper_str = context.perform_substitution(load_gripper)
+def generate_robot_state_publisher(context: LaunchContext, namespace, should_load_gripper, franka_hand, start_coords: tuple[float]):
+    load_gripper_str = context.perform_substitution(should_load_gripper)
     franka_hand_str = context.perform_substitution(franka_hand)
+    namespace = context.perform_substitution(namespace)
 
     franka_xacro_file = os.path.join(
         get_package_share_directory('franka_description'),
         'robots',
-        robot_type_str,
-        robot_type_str + '.urdf.xacro'
+        "fr3",
+        'fr3.urdf.xacro'
     )
 
     robot_description_config = xacro.process_file(
         franka_xacro_file,
         mappings={
-            'robot_type': robot_type_str,
             'hand': load_gripper_str,
             'ros2_control': 'true',
             'gazebo': 'true',
-            'ee_id': franka_hand_str
+            'ee_id': franka_hand_str,
+            "arm_prefix": namespace,
+            "xyz": f"{start_coords[0]} {start_coords[1]} {start_coords[2]}"
         }
     )
-
-    robot_description = {'robot_description': robot_description_config.toxml()}
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
+        namespace=namespace,
         output='both',
         parameters=[
-            robot_description,
+            {'robot_description': robot_description_config.toxml()},
         ]
     )
 
@@ -69,96 +69,157 @@ def get_robot_description(context: LaunchContext, robot_type, load_gripper, fran
 
 
 def generate_launch_description():
-    # Configure ROS nodes for launch
     load_gripper_name = 'load_gripper'
     franka_hand_name = 'franka_hand'
-    robot_type_name = 'robot_type'
-    namespace_name = 'namespace'
 
-
-    load_gripper = LaunchConfiguration(load_gripper_name)
+    # Launch args/configurations
+    should_load_gripper = LaunchConfiguration(load_gripper_name)
     franka_hand = LaunchConfiguration(franka_hand_name)
-    robot_type = LaunchConfiguration(robot_type_name)
-    namespace = LaunchConfiguration(namespace_name)
+    left_namespace = LaunchConfiguration("left_namespace")
+    right_namespace = LaunchConfiguration("right_namespace")
 
     load_gripper_launch_argument = DeclareLaunchArgument(
-            load_gripper_name,
-            default_value='false',
-            description='true/false for activating the gripper')
+        load_gripper_name,
+        default_value='true',
+        description='true/false for activating the gripper'
+    )
     franka_hand_launch_argument = DeclareLaunchArgument(
-            franka_hand_name,
-            default_value='franka_hand',
-            description='Default value: franka_hand')
-    robot_type_launch_argument = DeclareLaunchArgument(
-            robot_type_name,
-            default_value='fr3',
-            description='Available values: fr3, fp3 and fer')
+        franka_hand_name,
+        default_value='franka_hand',
+        description='Default value: franka_hand'
+    )
     namespace_launch_argument = DeclareLaunchArgument(
-        namespace_name,
-        default_value='',
-        description='Namespace for the robot. If not set, the robot will be launched in the root namespace.')
-
-    # Get robot description
-    robot_state_publisher = OpaqueFunction(
-        function=get_robot_description,
-        args=[robot_type, load_gripper, franka_hand])
+        "left_namespace",
+        default_value='left',
+        description='Namespace for the robot. If not set, the robot will be launched in the root namespace.'
+    )
+    namespace2_launch_argument = DeclareLaunchArgument(
+        "right_namespace",
+        default_value="right",
+        description="Namespace for da other robot"
+    )
 
     # Gazebo Sim
     os.environ['GZ_SIM_RESOURCE_PATH'] = os.path.dirname(get_package_share_directory('franka_description'))
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    gazebo_empty_world = IncludeLaunchDescription(
+    gazebo_package_dir = get_package_share_directory('ros_gz_sim')
+    world_file = os.path.join(get_package_share_directory("cs593"), "worlds", "lab_scene.world")
+    gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': 'empty.sdf -r', }.items(),
+            os.path.join(gazebo_package_dir, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': f'{world_file} -r', }.items(),
     )
 
-    # Spawn
-    spawn = Node(
+    # RVIZ
+    rviz_file = os.path.join(get_package_share_directory('cs593'), 'config', 'lab_scene.rviz')
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        namespace="",
+        arguments=['--display-config', rviz_file, '-f', 'world'],
+    )
+
+    # left arm
+    left_robot_state_publisher = OpaqueFunction(
+        function=generate_robot_state_publisher,
+        args=[left_namespace, should_load_gripper, franka_hand, (0, 0.5, 0)]
+    )
+
+    left_joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        namespace=left_namespace,
+        parameters=[
+            {'source_list': ['/joint_states'],
+             'rate': 30}
+        ]
+    )
+
+    left_spawn_node = Node(
         package='ros_gz_sim',
         executable='create',
-        namespace=namespace,
-        arguments=['-topic', '/robot_description'],
+        namespace=left_namespace,
+        arguments=['--topic', 'robot_description',
+                   '--name', f'{left_namespace}'
+        ],
         output='screen',
     )
 
-    # Visualize in RViz
-    rviz_file = os.path.join(get_package_share_directory('franka_description'), 'rviz',
-                             'visualize_franka.rviz')
-    rviz = Node(package='rviz2',
-             executable='rviz2',
-             name='rviz2',
-             namespace=namespace,
-             arguments=['--display-config', rviz_file, '-f', 'world'],
+    left_joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", 
+                   "--controller-manager", [left_namespace, "/controller_manager"],
+        ],
+        output="screen",
     )
 
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-                'joint_state_broadcaster'],
-        output='screen'
+    # right arm
+    right_robot_state_publisher = OpaqueFunction(
+        function=generate_robot_state_publisher,
+        args=[right_namespace, should_load_gripper, franka_hand, (0, -0.5, 0)]
     )
+
+    right_joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        namespace=right_namespace,
+        parameters=[
+            {'source_list': ['/joint_states'],
+             'rate': 30}
+        ]
+    )
+
+    right_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        namespace=right_namespace,
+        arguments=['--topic', 'robot_description',
+                   '--name', f'{right_namespace}'
+        ],
+        output='screen',
+    )
+
+    right_joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", 
+                   "--controller-manager", [right_namespace, "/controller_manager"],
+        ],
+        output="screen",
+    )
+
 
     return LaunchDescription([
         load_gripper_launch_argument,
         franka_hand_launch_argument,
-        robot_type_launch_argument,
         namespace_launch_argument,
-        gazebo_empty_world,
-        robot_state_publisher,
-        rviz,
-        spawn,
+        namespace2_launch_argument,
+
+        gazebo_launch,
+        rviz_node,
+
+        left_robot_state_publisher,
+        left_joint_state_publisher,
+        left_spawn_node,
         RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=spawn,
-                    on_exit=[load_joint_state_broadcaster],
-                )
+            event_handler=OnProcessExit(
+                target_action=left_spawn_node,
+                on_exit=[left_joint_state_broadcaster_spawner],
+            )
         ),
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            namespace=namespace,
-            parameters=[
-                {'source_list': ['joint_states'],
-                 'rate': 30}],
+
+        right_robot_state_publisher,
+        right_joint_state_publisher,
+        right_spawn_node,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=right_spawn_node,
+                on_exit=[right_joint_state_broadcaster_spawner],
+            )
         ),
+
     ])
