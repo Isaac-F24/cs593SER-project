c731f92179a4486095217c3563a957736cc8ba77
Isaac-F24 <isaacfuksman@gmail.com>
2026-04-27 15:48:56 -0400

    add move groups to launch file

--- cs593/launch/visualize_franka_robot.launch.py
+++ cs593/launch/visualize_franka_robot.launch.py
@@ -14,6 +14,7 @@
 
 import os
 import xacro
+import yaml
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -23,16 +24,16 @@ from launch.event_handlers import OnProcessExit
 
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch import LaunchContext, LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import  LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
 
-def generate_robot_state_publisher(context: LaunchContext, namespace, should_load_gripper, franka_hand, start_coords: tuple[float]):
-    load_gripper_str = context.perform_substitution(should_load_gripper)
-    franka_hand_str = context.perform_substitution(franka_hand)
+def generate_robot_state_publisher(context: LaunchContext, namespace, start_coords: tuple[float]):
+    load_gripper_str = 'true'
     namespace = context.perform_substitution(namespace)
 
     franka_xacro_file = os.path.join(
@@ -48,7 +49,7 @@ def generate_robot_state_publisher(context: LaunchContext, namespace, should_loa
             'hand': load_gripper_str,
             'ros2_control': 'true',
             'gazebo': 'true',
-            'ee_id': franka_hand_str,
+            'ee_id': "franka_hand",
             "arm_prefix": namespace,
             "xyz": f"{start_coords[0]} {start_coords[1]} {start_coords[2]}"
         }
@@ -69,25 +70,11 @@ def generate_robot_state_publisher(context: LaunchContext, namespace, should_loa
 
 
 def generate_launch_description():
-    load_gripper_name = 'load_gripper'
-    franka_hand_name = 'franka_hand'
 
     # Launch args/configurations
-    should_load_gripper = LaunchConfiguration(load_gripper_name)
-    franka_hand = LaunchConfiguration(franka_hand_name)
     left_namespace = LaunchConfiguration("left_namespace")
     right_namespace = LaunchConfiguration("right_namespace")
 
-    load_gripper_launch_argument = DeclareLaunchArgument(
-        load_gripper_name,
-        default_value='true',
-        description='true/false for activating the gripper'
-    )
-    franka_hand_launch_argument = DeclareLaunchArgument(
-        franka_hand_name,
-        default_value='franka_hand',
-        description='Default value: franka_hand'
-    )
     namespace_launch_argument = DeclareLaunchArgument(
         "left_namespace",
         default_value='left',
@@ -98,6 +85,66 @@ def generate_launch_description():
         default_value="right",
         description="Namespace for da other robot"
     )
+
+    # xacro files 
+    franka_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots',
+        "fr3",
+        'fr3.urdf.xacro'
+    )
+
+    franka_semantic_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        "robots", 
+        "fr3", 
+        "fr3.srdf.xacro"
+    )
+
+    # moveIt planning configuration
+    kinematics_yaml_content = load_yaml(
+        'franka_fr3_moveit_config', 'config/kinematics.yaml'
+    )
+
+    moveit_simple_controllers_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/fr3_controllers.yaml'
+    )
+    moveit_controllers_config = {
+        'moveit_simple_controller_manager': moveit_simple_controllers_yaml,
+        'moveit_controller_manager': 'moveit_simple_controller_manager'
+                                     '/MoveItSimpleControllerManager',
+    }
+
+    trajectory_execution_config = {
+        'moveit_manage_controllers': True,
+        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+        'trajectory_execution.allowed_goal_duration_margin': 0.5,
+        'trajectory_execution.allowed_start_tolerance': 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        'publish_planning_scene': True,
+        'publish_geometry_updates': True,
+        'publish_state_updates': True,
+        'publish_transforms_updates': True,
+    }
+
+    ompl_planning_pipeline_config = {
+        'move_group': {
+            'planning_plugin': 'ompl_interface/OMPLPlanner',
+            'request_adapters': 'default_planner_request_adapters/AddTimeOptimalParameterization '
+                                'default_planner_request_adapters/ResolveConstraintFrames '
+                                'default_planner_request_adapters/FixWorkspaceBounds '
+                                'default_planner_request_adapters/FixStartStateBounds '
+                                'default_planner_request_adapters/FixStartStateCollision '
+                                'default_planner_request_adapters/FixStartStatePathConstraints',
+            'start_state_max_bounds_error': 0.1,
+        }
+    }
+    ompl_planning_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/ompl_planning.yaml'
+    )
+    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
 
     # Gazebo Sim
     os.environ['GZ_SIM_RESOURCE_PATH'] = os.path.dirname(get_package_share_directory('franka_description'))
@@ -123,7 +170,7 @@ def generate_launch_description():
     # left arm
     left_robot_state_publisher = OpaqueFunction(
         function=generate_robot_state_publisher,
-        args=[left_namespace, should_load_gripper, franka_hand, (0, 0.5, 0)]
+        args=[left_namespace, (0, 0.5, 0)]
     )
 
     left_joint_state_publisher = Node(
@@ -156,10 +203,54 @@ def generate_launch_description():
         output="screen",
     )
 
+    left_xacro_file_content = ParameterValue(
+        Command([
+            "xacro ", franka_xacro_file,
+            " hand:=true",
+            " ros2_control:=true",
+            " gazebo:=true",
+            " ee_id:=franka_hand",
+            " arm_prefix:=", left_namespace,
+            " xyz:=", f"\"{0} {0.5} {0}\""
+        ]),
+        value_type=str
+    )
+
+    left_srdf_xacro_content = ParameterValue(
+        Command([
+            "xacro ", franka_semantic_xacro_file,
+            " hand:=true",
+            " ee_id:=franka_hand"
+            " arm_prefix:=", left_namespace,
+        ]),
+        value_type=str
+    )
+
+
+    left_move_group_node = Node(
+        package='moveit_ros_move_group',
+        executable='move_group',
+        namespace=left_namespace,
+        output='screen',
+        parameters=[
+            {
+                "robot_description": left_xacro_file_content
+            },
+            {
+                "robot_description_semantic": left_srdf_xacro_content
+            },
+            kinematics_yaml_content,
+            trajectory_execution_config,
+            moveit_controllers_config,
+            planning_scene_monitor_parameters,
+            ompl_planning_pipeline_config
+        ],
+    )
+
     # right arm
     right_robot_state_publisher = OpaqueFunction(
         function=generate_robot_state_publisher,
-        args=[right_namespace, should_load_gripper, franka_hand, (0, -0.5, 0)]
+        args=[right_namespace, (0, -0.5, 0)]
     )
 
     right_joint_state_publisher = Node(
@@ -192,10 +283,51 @@ def generate_launch_description():
         output="screen",
     )
 
+    right_xacro_file_content = ParameterValue(
+        Command([
+            "xacro ", franka_xacro_file,
+            " hand:=true",
+            " ros2_control:=true",
+            " gazebo:=true",
+            " ee_id:=franka_hand",
+            " arm_prefix:=", right_namespace,
+            " xyz:=", f"\"{0} {-0.5} {0}\""
+        ]),
+        value_type=str
+    )
+
+    right_srdf_xacro_content = ParameterValue(
+        Command([
+            "xacro ", franka_semantic_xacro_file,
+            " hand:=true",
+            " ee_id:=franka_hand"
+            " arm_prefix:=", right_namespace,
+        ]),
+        value_type=str
+    )
+
+
+    right_move_group_node = Node(
+        package='moveit_ros_move_group',
+        executable='move_group',
+        namespace=right_namespace,
+        output='screen',
+        parameters=[
+            {
+                "robot_description": right_xacro_file_content
+            },
+            {
+                "robot_description_semantic": right_srdf_xacro_content
+            },
+            kinematics_yaml_content,
+            trajectory_execution_config,
+            moveit_controllers_config,
+            planning_scene_monitor_parameters,
+            ompl_planning_pipeline_config
+        ],
+    )
 
     return LaunchDescription([
-        load_gripper_launch_argument,
-        franka_hand_launch_argument,
         namespace_launch_argument,
         namespace2_launch_argument,
 
@@ -205,6 +337,7 @@ def generate_launch_description():
         left_robot_state_publisher,
         left_joint_state_publisher,
         left_spawn_node,
+        left_move_group_node,
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=left_spawn_node,
@@ -215,6 +348,7 @@ def generate_launch_description():
         right_robot_state_publisher,
         right_joint_state_publisher,
         right_spawn_node,
+        right_move_group_node,
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=right_spawn_node,
@@ -223,3 +357,13 @@ def generate_launch_description():
         ),
 
     ])
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError: 
+        return None
